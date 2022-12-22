3756056699bd2b31454e7ea9cc3e7a066e42cfdd
Nguyễn Tuấn Anh <71357229+tuananh140620@users.noreply.github.com>
2022-12-22 18:24:15 +0700

    Overridin vs Overloading

--- src/Overriding/Main.java
+++ src/Overriding/Main.java
@@ -1,23 +1,4 @@
 package Overriding;
 
-
-import java.io.*;
-
-class Main {
-
-    static int add(int a, int b) {
-        return a + b;
-    }
-
-    static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
-    public static void main(String args[]) {
-        System.out.println("add() with 2 parameters");
-        System.out.println(add(4, 6));
-
-        System.out.println("add() with 3 parameters");
-        System.out.println(add(4, 6, 7));
-    }
+public class Main {
 }
